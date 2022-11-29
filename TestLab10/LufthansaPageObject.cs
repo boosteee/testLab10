961879282f0de020ef791e7725fce48bfb09001e
Daniil Kachalkin <cara.by9@gmail.com>
2022-11-29 18:56:55 +0300

    lab10 v2.1

--- TestLab10/LufthansaPageObject.cs
+++ TestLab10/LufthansaPageObject.cs
@@ -18,7 +18,7 @@ namespace TestLab10
         private IWebElement _targetFlightDate => _driver.FindElement(By.CssSelector("#dcep-a4a628f31-d438-4cc1-8523-5a6040309ec6-flightstatus-flightStatusByFlightNumberRequest\\.date-item-4"));
         private IWebElement _findButton => _driver.FindElement(By.CssSelector("#dcep-tab-control-overlay11-статусРейса-section > div > div > form > div.d-flex.button-flex > button"));
         private IWebElement _hotelButton => _driver.FindElement(By.CssSelector("#dcep-tab-control-standalone9-отель-toggle"));
-        private IWebElement _pointOfВestination => _driver.FindElement(By.CssSelector("#dcep-a10c1dd30-2a78-487a-9682-0065526a9424-flm-hotel-hotelQuery\\.cityName"));
+        private IWebElement _pointOfDestination => _driver.FindElement(By.CssSelector("#dcep-a10c1dd30-2a78-487a-9682-0065526a9424-flm-hotel-hotelQuery\\.cityName"));
         private IWebElement _calendar => _driver.FindElement(By.CssSelector("#dcep-tab-control-standalone9-отель-section > div > form > div:nth-child(1) > div:nth-child(2) > div > div.input-wrapper.only-child"));
         private IWebElement _firstDay => _driver.FindElement(By.CssSelector("body > div.modal.react-modal.modal-datepicker.modal-size-datepicker.react-modal-opened > div > div > div.modal-body > div.DayPicker.DayPicker_1.DayPicker__horizontal.DayPicker__horizontal_2 > div > div > div.DayPicker_focusRegion.DayPicker_focusRegion_1 > div.DayPicker_transitionContainer.DayPicker_transitionContainer_1.DayPicker_transitionContainer__horizontal.DayPicker_transitionContainer__horizontal_2 > div > div:nth-child(3) > div > table > tbody > tr:nth-child(1) > td:nth-child(5)"));
         private IWebElement _lastDay => _driver.FindElement(By.CssSelector("body > div.modal.react-modal.modal-datepicker.modal-size-datepicker.react-modal-opened > div > div > div.modal-body > div.DayPicker.DayPicker_1.DayPicker__horizontal.DayPicker__horizontal_2 > div > div > div.DayPicker_focusRegion.DayPicker_focusRegion_1 > div.DayPicker_transitionContainer.DayPicker_transitionContainer_1.DayPicker_transitionContainer__horizontal.DayPicker_transitionContainer__horizontal_2 > div > div:nth-child(3) > div > table > tbody > tr:nth-child(2) > td:nth-child(3)"));
@@ -40,8 +40,8 @@ namespace TestLab10
         {
             _acceptCookiesButton.Click();
             _hotelButton.Click();
-            _pointOfВestination.Click();
-            _pointOfВestination.SendKeys(city);
+            _pointOfDestination.Click();
+            _pointOfDestination.SendKeys(city);
             _calendar.Click();
             _firstDay.Click();
             _lastDay.Click();
